c4a6dc105f683d06f1660b857a1524dbdc5c13f3
wllw <liu1351853931@outlook.com>
2019-04-09 14:07:35 +0800

    add user info into User table

--- backend/user/models.py
+++ backend/user/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class User(models.Model):
     account = models.CharField(max_length=32, unique=True)
     password = models.CharField(max_length=64)
+    name = models.CharField(max_length=32)
+    age = models.IntegerField(default=0)
+    student_id = models.CharField(max_length=32)
+    sex = models.CharField(max_length=32)
+    major = models.CharField(max_length=128)
+    grade = models.CharField(max_length=32)
     c_time = models.DateTimeField(auto_now_add=True)  # 保存用户创建时间
 
     def __str__(self):
