c70349be7fcc43231959d1cf2ff5175356be7472
wllw <liu1351853931@outlook.com>
2019-04-10 14:43:20 +0800

    add post model

--- backend/demand/models.py
+++ backend/demand/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=32)
+    post_detail = models.TextField()
+    request_num = models.IntegerField()
+    accept_num = models.IntegerField(default=0)
+    deadline = models.DateField()
+    post_time = models.DateTimeField(auto_now_add=True)  # 发布时间，会自动添加
+    if_end = models.BooleanField(default=False)
+    poster = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["post_time"]
