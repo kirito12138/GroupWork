3413d6b8ef700651f51d17464c8681a98cf91b4b
wllw <liu1351853931@outlook.com>
2019-04-10 21:20:19 +0800

    add get_unclosed_posts view

--- backend/demand/views.py
+++ backend/demand/views.py
@@ -1,9 +1,10 @@
 import json
 import re
-from django.http import JsonResponse
-from demand import models
+from django.http import JsonResponse, HttpResponse
+from demand.models import Post
 from user.jwt_token import verify_token
 import datetime
+from django.core import serializers
 
 length = re.compile("^.{1,20}$")
 yyyy_mm_dd = re.compile("^\d\d\d\d-\d\d-\d\d$")
@@ -38,16 +39,16 @@ def post(request):
         return JsonResponse({'ret': False, 'error_code': 3})
 
     # 新建发布校验
-    if models.Post.objects.filter(title=title, post_detail=post_detail, request_num=request_num, deadline=deadline,
-                                  poster__account=account).exists():
+    if Post.objects.filter(title=title, post_detail=post_detail, request_num=request_num, deadline=deadline,
+                           poster__account=account).exists():
         return JsonResponse({'ret': False, 'error_code': 4})
 
-    new_post = models.Post.objects.create()
+    new_post = Post.objects.create()
     new_post.title = title
     new_post.post_detail = post_detail
     new_post.request_num = request_num
     new_post.deadline = deadline
-    new_post.poster = models.Post.objects.get(account=account)
+    new_post.poster = Post.objects.get(account=account)
     new_post.save()
 
     return JsonResponse({'ret': True, 'postID': str(new_post.id)})
@@ -57,9 +58,11 @@ def get_unclosed_posts(request):
     if request.method != "GET":
         return JsonResponse({'ret': False, 'error_code': 1})
 
-    token = request.META.get('AUTHORIZATION')
-    if not token:
-        return JsonResponse({'ret': False, 'error_code': 5})
-    account = verify_token(token)
+    account = verify_token(request.META.get('AUTHORIZATION'))
     if not account:
         return JsonResponse({'ret': False, 'error_code': 5})
+
+    data = serializers.serialize('json', Post.objects.filter(if_end=False).order_by('-post_time'),
+                                 fields=(
+                                 'title', 'post_detail', 'request_num', 'accept_num', 'deadline', 'id', 'poster'))
+    return HttpResponse(json.dumps(data), content_type="application/json")
--- backend/user/jwt_token.py
+++ backend/user/jwt_token.py
@@ -1,6 +1,5 @@
 import jwt
 from datetime import datetime, timedelta
-
 from backend.settings import SECRET_KEY
 
 
