4bd7864a764bafc608e93ba52641e39884ab1760
wllw <liu1351853931@outlook.com>
2019-04-11 17:46:14 +0800

    add apply model

--- backend/demand/models.py
+++ backend/demand/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+def resume_file_path(instance, filename):
+    # 文件上传到MEDIA_ROOT/resume/<filename>目录中
+    return 'resume/{1}.{2}'.format(instance.id, filename.split('.')[-1])
+
+
 class Post(models.Model):
     title = models.CharField(max_length=32)
     post_detail = models.TextField()
@@ -16,3 +21,10 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["post_time"]
+
+
+class Apply(models.Model):
+    resume = models.FileField(max_length=256, upload_to=resume_file_path)
+    status = models.CharField(max_length=32, default="pending")  # pending(待定) accept(接受), end(结束)
+    post = models.ForeignKey('Post', on_delete=models.SET_NULL, null=True)
+    applicant = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True)
