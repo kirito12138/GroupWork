be92bf884ad671fef11619d6e66383ba3490336d
David Lau <liu1351853931@outlook.com>
2019-05-11 22:29:38 +0800

    change get_openid

--- backend/user/api_wechat.py
+++ backend/user/api_wechat.py
@@ -6,8 +6,12 @@ from backend.settings import APP_ID, APP_SECRET, APP_ID_URL
 def get_openid(js_code):
     url = APP_ID_URL + "?appid=" + APP_ID + "&secret=" + APP_SECRET \
           + "&js_code=" + js_code + "&grant_type=authorization_code"
-    r = requests.get(url)
-    data = r.json()
+    try:
+        # r = requests.get(url)
+        r = requests.get(url, timeout=10)
+        data = r.json()
+    except:
+        return None
     if 'errcode' not in data or data['errcode'] == 0:
         return data['openid']
     return None
