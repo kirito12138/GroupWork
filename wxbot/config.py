6c97020ba097393b06dfbb4a173981459cc573e7
awfsgdf <834973816@qq.com>
2019-05-16 01:27:06 +0800

    1

---
+++ wxbot/config.py
@@ -0,0 +1,30 @@
+"""项目配置"""
+
+# 图灵机器人，99元一月付费版，尽情享用！
+tuling_api_key = '88f17f853d974387af64955bed9466f4'
+
+# 自动回复
+is_friend_auto_reply = False  # 好友自动回复
+is_group_reply = False  # 此项表示群中是否回复
+is_group_at_reply = False  # 上一项开启后此项才生效
+is_forward_revoke_msg = False  # 开启防撤回模式
+is_forward_group_at_msg = False  # 转发群@我的消息
+
+# 机器人主人
+bot_master_name = ''  # 使用备注名更安全，只允许一个，可远程控制机器人，如果不设置(空)则将文件助手设置为管理员，但不具备远程控制功能
+
+# 监听某些好友群聊，如老板
+is_listen_friend = True
+listen_friend_names = '水'  # 需要监听的人名称，使用备注名更安全，允许多个用|分隔，如：主管|项目经理|产品狗
+listen_friend_groups = '测试'  # 在这些群里监听好友说的话，匹配模式：包含“唯一集团工作群”的群
+
+
+# 转发信息至群
+is_forward_mode = False  # 打开转发模式，主人发送给机器人的消息都将转发至forward_groups群
+forward_groups = '测试'  # 需要将消息转发的群，匹配模式同上
+
+# 群分享监控
+is_listen_sharing = False
+listen_sharing_groups = 'Python新手交流'  # 监控群分享，匹配模式同上
+
+
---
+++ wxbot/robot.py
@@ -0,0 +1,82 @@
+from wxpy import *
+
+import wx_reply
+import wx_command
+import load
+
+
+# 微信机器人，缓存登录信息，console_qr表示在控制台打出二维码，部署到服务器时需要加上
+# 如果你不需要部署到服务器中，可以去掉console_qr=True这个参数
+bot = Bot(cache_path=True, console_qr=False)
+# 加载配置信息到机器人
+load.load_config_to_bot(bot)
+
+
+"""好友功能"""
+@bot.register(msg_types=FRIENDS)
+def auto_accept_friends(msg):
+    """自动接受好友请求"""
+    wx_reply.auto_accept_friends(msg)
+
+
+@bot.register(chats=Friend)
+def friend_msg(msg):
+    """接收好友消息"""
+    if not msg.bot.is_friend_auto_reply:
+        return None
+    if msg.type == TEXT:
+        wx_reply.auto_reply(msg)
+        return None
+    elif msg.type == RECORDING:
+        return '不停不停，王八念经'
+    else:
+        pass
+
+
+"""群功能"""
+@bot.register(chats=Group)
+def group_msg(msg):
+    """接收群消息"""
+    print(1)
+    # 群@转发功能
+    if msg.is_at and msg.bot.is_forward_group_at_msg:
+        msg.forward(msg.bot.master, prefix='「{0}」在群「{1}」中艾特了你：'.format(msg.member.name, msg.chat.name))
+
+    if msg.type == TEXT:
+        # 群回复
+        if msg.bot.is_group_reply:
+            if msg.bot.is_group_at_reply:
+                # @机器人才回复
+                if msg.is_at:
+                    wx_reply.auto_reply(msg)
+            else:
+                # 不用@直接回复
+                wx_reply.auto_reply(msg)
+    elif msg.type == SHARING and msg.bot.is_listen_sharing and msg.chat in msg.bot.listen_sharing_groups:
+        # 群分享转发监控，防止分享广告
+        msg.forward(msg.bot.master, prefix='分享监控：「{0}」在「{1}」分享了：'.format(msg.member.name, msg.chat.name))
+    else:
+        pass
+    # 监听好友群聊，如老板讲话
+    if msg.bot.is_listen_friend and msg.chat in msg.bot.listen_friend_groups and msg.member.is_friend in msg.bot.listen_friends:
+        print('ok')
+        msg.forward(msg.bot.master, prefix='监听指定好友群消息：「{0}」在「{1}」发了消息：'.format(msg.member.is_friend.remark_name or msg.member.nick_name, msg.chat.name))
+    return None
+
+
+@bot.register(msg_types=NOTE)
+def system_msg(msg):
+    """接收系统消息"""
+    wx_reply.handle_system_msg(msg)
+
+
+"""管理员功能"""
+@bot.register(chats=bot.master)
+def do_command(msg):
+    """执行管理员命令"""
+    wx_command.do_command(msg)
+
+
+# 堵塞进程，直到结束消息监听 (例如，机器人被登出时)
+# embed() 互交模式阻塞，电脑休眠或关闭互交窗口则退出程序
+bot.join()
