0afd434d2d11232df073f33cf5c8d5b396231d88
David Lau <liu1351853931@outlook.com>
2019-05-28 16:35:34 +0800

    将数据库修改为mysql

--- backend/backend/settings.example.py
+++ backend/backend/settings.example.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',  # 数据库名字
+        'USER': '',  # 账号
+        'PASSWORD': '',  # 密码
+        'HOST': '127.0.0.1',  # 本机地址
+        'PORT': '3306',  # 端口
     }
 }
 
