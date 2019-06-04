ffd19a221fa505d55086f7cc4f2b10930ed9b44b
Cookize <zy.song@qq.com>
2019-06-04 21:05:06 +0800

    完成用户查看邀请信息和被邀请信息部分代码

--- backend/Team/views.py
+++ backend/Team/views.py
@@ -29,3 +29,42 @@ def invite_user(request, user_id):
         return JsonResponse({'ret': True})
     old.state = 0
     return JsonResponse({'ret': True})
+
+def invitee_get_invitation(request):
+    if request.method != 'GET':
+        return JsonResponse({'ret': False, 'error_code': 1})
+
+    user = verify_token(request.META.get('HTTP_AUTHORIZATION'))
+    if not user:
+        return JsonResponse({'ret': False, 'error_code': 5})
+
+    invitations = Invitation.objects.filter(invitee=user, state=0)
+    ret_data = []
+    for invitation in invitations:
+        ret_data.append({
+            'name': invitation.inviter.name,
+            'avatar': invitation.inviter.avatar_url,
+        #     TODO 增加被邀请信息页面需要显示的邀请者信息
+        })
+
+    return JsonResponse(ret_data, safe=False)
+
+def inviter_get_invitation(request):
+    if request.method != 'GET':
+        return JsonResponse({'ret': False, 'error_code': 1})
+
+    user = verify_token(request.META.get('HTTP_AUTHORIZATION'))
+    if not user:
+        return JsonResponse({'ret': False, 'error_code': 5})
+
+    invitations = Invitation.objects.filter(inviter=user)
+    ret_data = []
+    for invitation in invitations:
+        ret_data.append({
+            'name': invitation.inviter.name,
+            'avatar': invitation.inviter.avatar_url,
+            #     TODO 增加邀请信息页面需要显示的被邀请者信息
+            'state': invitation.state,
+        })
+
+    return JsonResponse(ret_data, safe=False)
