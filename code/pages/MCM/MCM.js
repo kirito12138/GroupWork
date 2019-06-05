af349535bf35ceb8655380c47080abe3ab5cb397
Wang Bing <wbkingice@sina.com>
2019-06-05 22:42:02 +0800

    修复美赛页面

--- code/pages/MCM/MCM.js
+++ code/pages/MCM/MCM.js
@@ -1,4 +1,5 @@
 // pages/MCM/MCM.js
+const { $Toast } = require('../../vant-weapp/dist/base/index');
 Page({
 
   /**
@@ -6,8 +7,47 @@ Page({
    */
   data: {
     searchValue: "",
+    is_fill: true,
+    actions5: [
+      {
+        name: '取消'
+      },
+      {
+        name: '填写',
+        color: '#ed3f14',
+        loading: false
+      }
+    ],
+    visible: false,
+    partens: [],
+    team: [],
   },
 
+  cancel_fil: function (e) {
+    if (e.detail.index == 0) {
+      this.gotoHome(e)
+    }
+    else {     
+      this.fil(e)
+    }
+  },
+
+  gotoHome: function(e) {
+    wx.reLaunch({
+      url: '../home/home',
+    })
+    this.setData({
+      visible: false
+    })
+  },
+  fil: function(e) {
+    wx.reLaunch({
+      url: '../home/home',
+    })
+    this.setData({
+      visible: false
+    })
+  },
 
   inputser: function (e) {
 
@@ -31,22 +71,33 @@ Page({
   choose: function(e)
   {
     console.log(e)
-    if(e.detail.x < 200)
+    var wid = this.data.windowWidth;
+    if(e.detail.x < (wid/2))
     {
       upQue();
     }
-    else if(e.detail.x >=200)
+    else if (e.detail.x >= (wid / 2))
     {
       newone();
     }
   },
 
-  newone: function(e)
+  newone: function()
   {
-    
+
   },
 
-  upQue: function(e)
+  upQue: function()
+  {
+
+  },
+
+  fillSurvey: function(e)
+  {
+    //填写问卷
+  },
+
+  invite: function(e)
   {
 
   },
@@ -70,18 +121,20 @@ Page({
       }
     })
 
+    var wid = this.data.windowWidth;
+
     const ctx = wx.createCanvasContext('myCanvas')
 
     ctx.moveTo(10, 0)
     ctx.lineTo(115, 0)
     ctx.lineTo(145,30)
-    ctx.lineTo(355,30)
-    ctx.lineTo(355,35)
+    ctx.lineTo(wid,30)
+    ctx.lineTo(wid,35)
     ctx.lineTo(0, 35)
     ctx.lineTo(0,10)
     ctx.arc(0 + 10, 0 + 10, 10, Math.PI, Math.PI * 1.5)
 
-    ctx.setFillStyle('yellow')
+    ctx.setFillStyle('blue')
     ctx.fill()
 
     ctx.setFillStyle('black')
@@ -93,8 +146,8 @@ Page({
     const bot = wx.createCanvasContext('bottcan')
     
     bot.moveTo(0, 0)
-    bot.lineTo(170, 0)
-    bot.lineTo(200, 35)
+    bot.lineTo(wid/2-15, 0)
+    bot.lineTo(wid / 2 + 15, 35)
     bot.lineTo(10, 35)
     bot.arc(0 + 10, 35 - 10, 10, Math.PI * 0.5, Math.PI)
     bot.setFillStyle('yellow')
@@ -105,20 +158,93 @@ Page({
 
 
     bot.beginPath()
-    bot.moveTo(170, 0)
-    bot.lineTo(353, 0)
-    bot.lineTo(353, 25)
-    bot.arc(353 - 10, 35 - 10, 10, 0, Math.PI * 0.5)
-    bot.lineTo(200, 35)
-    bot.setFillStyle('green')
+    bot.moveTo(wid / 2 - 15, 0)
+    bot.lineTo(wid, 0)
+    bot.lineTo(wid, 25)
+    bot.arc(wid - 10, 35 - 10, 10, 0, Math.PI * 0.5)
+    bot.lineTo(wid / 2 + 15, 35)
+    bot.setFillStyle('blue')
     bot.fill()
     bot.setFillStyle('black')
     bot.setFontSize(20)
     bot.fillText('换一批', 230, 25)
 
-
-
     bot.draw()
+
+    const intr = wx.createCanvasContext('intr')
+    intr.moveTo(wid-115, 0)
+    intr.lineTo(wid-10, 0)
+    intr.arc(wid - 10, 0 + 10, 10, Math.PI * 1.5, Math.PI * 2)
+    intr.lineTo(wid, 35)
+    
+    intr.lineTo(0, 35)
+    intr.lineTo(0,30)
+    intr.lineTo(wid-145,30)
+    
+
+    intr.setFillStyle('blue')
+    intr.fill()
+
+    intr.setFillStyle('black')
+    intr.setFontSize(20)
+    intr.fillText('推荐队友', wid-110, 25)
+
+    intr.draw()
+
+    const quit = wx.createCanvasContext('bottcan1')
+    quit.beginPath()
+    quit.moveTo(wid / 2 - 15, 0)
+    quit.lineTo(wid, 0)
+    quit.lineTo(wid, 25)
+    quit.arc(wid - 10, 35 - 10, 10, 0, Math.PI * 0.5)
+    quit.lineTo(wid / 2 + 15, 35)
+    quit.setFillStyle('red')
+    quit.fill()
+    quit.setFillStyle('black')
+    quit.setFontSize(20)
+    quit.fillText('退出队伍', 230, 25)
+
+    quit.draw()
+
+    const _jwt = wx.getStorageSync('jwt');
+    var _history = wx.getStorageSync('history');
+    var tk;
+
+    if (_jwt) {
+      tk = JSON.parse(_jwt);
+      console.log(tk);
+    }
+    else {
+      console.log("no token");
+      return;
+    }
+
+    $Toast({
+      content: '加载中',
+      type: 'loading',
+      duration: 0
+    });
+
+    wx.request({
+      url: 'https://group.tttaaabbbccc.club/f/processing/',
+      method: "POST",
+      data: {
+        history: _history,
+      },
+      header: {
+        "Content-Type": "application/json;charset=UTF-8",
+        'Authorization': tk
+      },
+      data: {
+        history: '0'
+      },
+      success(res) {
+        $Toast.hide()
+      },
+      fail(res) {
+        $Toast.hide();
+      }
+    })
 
 
   },
