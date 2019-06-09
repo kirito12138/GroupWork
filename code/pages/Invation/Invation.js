752ff11aceb53df54b627767cf3c9249fba79d7d
shihaobai <1798930569@qq.com>
2019-06-09 16:33:05 +0800

    接受邀请 函数的实现

--- code/pages/Invation/Invation.js
+++ code/pages/Invation/Invation.js
@@ -1,22 +1,144 @@
 // pages/Invation/Invation.js
 const { $Toast } = require('../../vant-weapp/dist/base/index');
+const { $Message } = require('../../vant-weapp/dist/base/index');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    f_posts:[1,2,3,4],
-    isShow:false
+    isShow:false,
+    visible:false,
+    actions5: [
+      {
+        name: '取消'
+      },
+      {
+        name: '确定',
+        color: '#ed3f14',
+        loading: false
+      }
+    ]
   },
   changeShow:function(e)
   {
     var i = e.currentTarget.dataset.index;
+
     let string = "f_posts[" + i + "].isShow";
     this.setData({
      [string]:!this.data.f_posts[i].isShow
     })
-    console.log(this.data.isShow)
+    console.log(this.data.f_posts[i].isShow)
+  },
+  cancelAccept: function (e) {
+    console.log("取消")
+    if (e.detail.index == 0) {
+      this.setData({
+        visible: false
+      })
+    }
+    else {
+      this.setData({
+        visible: false
+      })
+      this.accept()
+    }
+  },
+  isAccept:function(e)
+  {
+    var i = e.currentTarget.dataset.index;
+    console.log(i,this.data.f_posts[i])
+    if(this.data.myId != this.data.f_posts[i].team_id)
+    {
+      this.setData({
+        visible: true,
+        inviteId:this.data.f_posts[i].id
+      });
+    }
+    else
+    {
+      $Message({
+        content: '您已加入该队伍',
+        type: 'error'
+      });
+    }
+  },
+  accept:function()
+  {
+    
+    var that = this;
+    const _jwt = wx.getStorageSync('jwt');
+    var tk;
+    if (_jwt) {
+      tk = JSON.parse(_jwt);
+    }
+    else {
+      return;
+    }
+    $Toast({
+      content: '加载中',
+      type: 'loading',
+      duration: 0
+    });
+    console.log(this.data.inviteId)
+    wx.request({
+      url: 'https://group.tttaaabbbccc.club/mcm/accept/' + this.data.inviteId+'/',
+      method: "GET",
+      header: {
+        "Content-Type": "application/json;charset=UTF-8",
+        'Authorization': tk
+      },
+      success(res) {
+        $Toast.hide()
+        if(res.data.ret)
+        {
+          $Message({
+            content: '加入成功',
+            type: 'success'
+          });
+        }
+        else
+        {
+          if(res.data.error_code == 2)
+          {
+            $Message({
+              content: '队伍已满',
+              type: 'error'
+            });
+          }
+          else if(res.data.error_code == 3)
+          {
+            $Message({
+              content: '邀请信息缺失',
+              type: 'error'
+            });
+          }
+          else if(res.data.error_code == 4)
+          {
+            $Message({
+              content: '本用户已有队友且本人为队长，不能退队',
+              type: 'error'
+            });
+            
+          }
+          else if(res.data.error_code == 5)
+          {
+            $Message({
+              content: '登陆状态已失效',
+              type: 'error'
+            });
+          }
+          
+        }
+        that.onLoad()
+
+      },
+      fail(res) {
+        $Toast.hide();
+      }
+    })
+
+
   },
   /**
    * 生命周期函数--监听页面加载
@@ -39,7 +161,7 @@ Page({
     });
 
     wx.request({
-      url: 'https://group.tttaaabbbccc.club/mcm/invitations/send//',
+      url: 'https://group.tttaaabbbccc.club/mcm/invitations/received//',
       method: "GET",
       header: {
         "Content-Type": "application/json;charset=UTF-8",
@@ -48,9 +170,14 @@ Page({
       success(res) {
         $Toast.hide()
         that.setData({
-          f_posts: res.data
+          f_posts: res.data.slice(1)
         });
-        console.log(that.data.f_posts)
+        that.setData(
+          {
+            myId:res.data[0].team_id
+          }
+        )
+        console.log('dasdas',that.data.f_posts)
 
       },
       fail(res) {
