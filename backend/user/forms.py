ebbf44c535a32f7140b697c11943b5ef4d57de28
root <root@localhost.localdomain>
2019-10-21 00:15:45 +0800

    修改账户为账号

--- backend/user/forms.py
+++ backend/user/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    account = forms.CharField(label="账户", max_length=40, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    account = forms.CharField(label="账号", max_length=40, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label="密码", max_length=64, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
